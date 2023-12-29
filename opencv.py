6cbd37c7ba8e73b9783f03bc3d662b0304f01bf0
Favour Odiyo <enezefavour@gmail.com>
2023-12-29 01:01:16 +0100

    Delete duplicate photos captured

--- opencv.py
+++ opencv.py
@@ -32,20 +32,51 @@ class CaptureVideo:
         self.stopped = True
         self.stream.release
 
-
-# Capture Image - Incomplete
-class CaptureImage:
-    def take_photo(frame):
-        now = datetime.now()
-        dt_string = now.strftime("%d%m%Y%H%M%S")
-        img_name = "{}.png".format(dt_string)
+class Duplicate:
+    def duplicate():
         img_dir = 'Pictures'
-        path = os.path.join(img_dir, img_name)
-        cv2.imwrite(path, frame)
-        return path
+        current_names = []
+        current_encodings = []
+
+        for images in os.listdir(img_dir):
+            image = face_recognition.load_image_file(os.path.join(img_dir, images))
+            face_encodings = face_recognition.face_encodings(image)[0]
+            current_encodings.append(face_encodings)
+            current_names.append(images)
+       
+        count = 0
+        faces_to_delete = []
+        while count < len(current_encodings):
+            temp_array = list(current_encodings)
+            temp_array.pop(count)
+
+            same = face_recognition.compare_faces(temp_array, current_encodings[count])
+
+            if True in same:
+                filepath = os.path.join(img_dir, current_names[count])
+                # print(filepath)
+                # os.remove(filepath)
+                # faces_to_delete.append(known_face_names[count])
+                # known_face_names.pop(count)
+                # known_face_encodings.pop(count)
+
+            face_distances = face_recognition.face_distance(temp_array, current_encodings[count])
+            best_match_index = np.argmin(face_distances)
+            if same[best_match_index]:
+                filepath2 = os.path.join(img_dir, current_names[count])
+                os.remove(filepath2)
+                faces_to_delete.append(current_names[count])
+                current_names.pop(count)
+                current_encodings.pop(count)
+
+            count = count + 1
+        known_face_encodings = current_encodings
+        known_face_names = current_names
+        return known_face_encodings, known_face_names
+
 
 def main():
-
+    known_face_encodings, known_face_names = Duplicate.duplicate()
     capture = CaptureVideo().start()  # Start the capturing thread
     
     while True:
